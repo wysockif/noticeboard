3adcc41a74c0e18dc2e98a1efa575e6197dd7115
Franek <wysocki.f.00@gmail.com>
2021-03-28 15:27:04 +0200

    Create a file structure

---
+++ front-end/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>noticeboard</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
