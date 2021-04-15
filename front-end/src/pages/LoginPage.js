6ac8c4a732b9c75754fece5386d54580e425e80c
Franek <wysocki.f.00@gmail.com>
2021-04-15 21:18:38 +0200

    Display the error message after login failure

--- front-end/src/pages/LoginPage.js
+++ front-end/src/pages/LoginPage.js
@@ -1,22 +1,40 @@
 import React, { Component } from 'react';
 import InputWithValidation from '../components/InputWithValidation';
-import { Container, Button /*, Spinner*/ } from 'react-bootstrap';
+import { Container, Button, Alert /*, Spinner*/ } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 export class LoginPage extends Component {
     state = {
         username: '',
-        password: ''
+        password: '',
+        alertMessage: undefined
+    }
+
+    onClickLoginButton = () => {
+        const body = {
+            username: this.state.username,
+            password: this.state.password
+        }
+        this.props.actions.postLogin(body)
+            .catch(error => {
+                if (error.response) {
+                    this.setState({ alertMessage: error.response.data.message })
+                }
+            });
     }
 
     onChangeUsername = event => {
-        this.setState({ username: event.target.value });
+        this.setState({ username: event.target.value, alertMessage: '' });
     }
 
     onChangePassword = event => {
-        this.setState({ password: event.target.value });
+        this.setState({ password: event.target.value, alertMessage: '' });
     }
 
     render() {
+        const disableSubmit = this.state.username && this.state.password ? false : true;
+
         return (
             <Container className="col-11 col-sm-9 col-md-7 col-lg-5 col-xl-4">
                 <h1 className="text-center my-4">Zaloguj się</h1>
@@ -36,10 +54,15 @@ export class LoginPage extends Component {
                 // error={this.state.errors.password}
                 />
 
+                {this.state.alertMessage && <Alert variant="danger" className="text-center">
+                    {this.state.alertMessage}
+                    <FontAwesomeIcon icon={["far", "frown-open"]} className="ms-1" />
+                </Alert>}
+
                 <div className="mb-3 text-center" >
                     <Button style={{ backgroundColor: '#B84' }} variant="outline-light"
-                    // onClick={this.onClickRegister}
-                    // disabled={this.state.pendingApiCall || !this.state.isPasswordRepeatCorrect}
+                        onClick={this.onClickLoginButton}
+                        disabled={disableSubmit}
                     >
                         Zaloguj się
                         {/* {this.state.pendingApiCall && <Spinner animation="border" size="sm" role="status" className="ms-1">
@@ -47,9 +70,15 @@ export class LoginPage extends Component {
                         </Spinner>} */}
                     </Button>
                 </div>
+
+
             </Container>
         )
     }
 }
-
+LoginPage.defaultProps = {
+    actions: {
+        postLogin: () => new Promise((resolve, reject) => { })
+    }
+}
 export default LoginPage;
--- front-end/src/pages/LoginPage.spec.js
+++ front-end/src/pages/LoginPage.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, waitFor, findByText } from '@testing-library/react';
 import { LoginPage } from './LoginPage';
 import '@testing-library/jest-dom/extend-expect';
 
@@ -64,10 +64,22 @@ describe('LoginPage', () => {
                 }
             };
         }
+        let usernameInput, passwordInput, button;
+
+        const setup = props => {
+            const rendered = render(<LoginPage {...props} />);
+            const { container, queryByPlaceholderText } = rendered;
+            usernameInput = queryByPlaceholderText('Nazwa użytkownika');
+            passwordInput = queryByPlaceholderText('Hasło');
+            button = container.querySelector('button');
+            fireEvent.change(usernameInput, changeEvent('newusername'));
+            fireEvent.change(passwordInput, changeEvent('newPassword123'));
+            return rendered;
+        }
         it('sets the username value into state', () => {
             // given
             const { queryByPlaceholderText } = render(<LoginPage />);
-            const usernameInput = queryByPlaceholderText('Nazwa użytkownika')
+            const usernameInput = queryByPlaceholderText('Nazwa użytkownika');
             // when
             fireEvent.change(usernameInput, changeEvent('newusername'));
             // then
@@ -77,12 +89,101 @@ describe('LoginPage', () => {
         it('sets the password value into state', () => {
             // given
             const { queryByPlaceholderText } = render(<LoginPage />);
-            const passwordInput = queryByPlaceholderText('Hasło')
+            const passwordInput = queryByPlaceholderText('Hasło');
             // when
             fireEvent.change(passwordInput, changeEvent('newPassword123'));
             // then
             expect(passwordInput).toHaveValue('newPassword123');
         });
+
+        it('calls postLogin when input fields are not empty', () => {
+            // given
+            const actions = {
+                postLogin: jest.fn().mockResolvedValue({})
+            }
+            setup({ actions });
+            // when
+            fireEvent.click(button);
+            // then
+            expect(actions.postLogin).toHaveBeenCalledTimes(1);
+        });
+
+        it('calls postLogin with username & password', () => {
+            // given
+            const actions = {
+                postLogin: jest.fn().mockResolvedValue({})
+            }
+            setup({ actions });
+            // when
+            fireEvent.click(button);
+            // then
+            const expectedBody = {
+                username: 'newusername',
+                password: 'newPassword123'
+            }
+            expect(actions.postLogin).toHaveBeenCalledWith(expectedBody);
+        });
+
+        it('displays message when login fails', async () => {
+            // given
+            const actions = {
+                postLogin: jest.fn().mockRejectedValue({
+                    response: {
+                        data: {
+                            message: 'Login failed'
+                        }
+                    }
+                })
+            }
+            const { queryByText, findByText } = setup({ actions });
+            // when
+            fireEvent.click(button);
+            // then
+            await waitFor(() => findByText('Login failed'));
+            const message = queryByText('Login failed');
+            expect(message).toBeInTheDocument();
+        });
+
+        it('does not throw error when actions not provided in props', () => {
+            // given
+            const { container, queryByPlaceholderText } = setup();
+            // when
+            const clicking = () => fireEvent.click(button);
+            // then
+            expect(clicking).not.toThrow();
+        });
+
+        it('enables the button when username and password is not empty', () => {
+            // given
+            setup();
+            // when
+            fireEvent.change(usernameInput, changeEvent('notEmptyUsername'));
+            fireEvent.change(passwordInput, changeEvent('notEmptyPassword123'));
+            // then
+            expect(button).not.toBeDisabled();
+        });
+
+        it('disables the button when username is empty', () => {
+            // given
+            setup();
+            // when
+            fireEvent.change(usernameInput, changeEvent(''));
+            fireEvent.change(passwordInput, changeEvent('notEmptyPassword123'));
+            // then
+            expect(button).toBeDisabled();
+        });
+
+        it('disables the button when password is empty', () => {
+            // given
+            setup();
+            // when
+            fireEvent.change(usernameInput, changeEvent('notEmptyUsername'));
+            fireEvent.change(passwordInput, changeEvent(''));
+            // then
+            expect(button).toBeDisabled();
+        });
+
+ 
     });
 });
 
