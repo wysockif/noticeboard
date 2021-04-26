fa5c89a856a149f74f8710641906ab1c99bbcf9b
Franek <wysocki.f.00@gmail.com>
2021-04-26 16:46:01 +0200

    Fix the problem of displaying text out of the designed area

--- front-end/src/components/profile-page/UserProfilePageCollapse.js
+++ front-end/src/components/profile-page/UserProfilePageCollapse.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Collapse, Container} from 'react-bootstrap';
 import InputWithValidation from '../InputWithValidation';
 import ButtonWithSpinner from '../ButtonWithSpinner';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 
 class UserProfilePageCollapse extends Component {
@@ -55,37 +56,45 @@ class UserProfilePageCollapse extends Component {
                 <Collapse in={this.props.open} onExited={this.onExitedCollapse}>
                     <div id="example-collapse-text" className="mt-3">
                         <Container className="col-12 col-sm-11 col-md-10 col-lg-9 col-xl-8">
-                            <div className="mb-2">Dane osobowe</div>
+                            <div className="mb-2">
+                                <FontAwesomeIcon icon="user" className="me-1"/>
+                                Dane osobowe
+                            </div>
                             <InputWithValidation
-                                label="Imię" placeholder="Imię" width="115px"
+                                label="Imię:" placeholder="Imię" width="115px"
                                 value={this.state.firstName}
                                 onChange={this.onChangeFirstName}
                                 hasError={this.props.errors.firstName !== undefined}
                                 error={this.props.errors.firstName}
                             />
                             <InputWithValidation
-                                label="Nazwisko" placeholder="Nazwisko" width="115px"
+                                label="Nazwisko:" placeholder="Nazwisko" width="115px"
                                 value={this.state.lastName}
                                 onChange={this.onChangeLastName}
                                 hasError={this.props.errors.lastName !== undefined}
                                 error={this.props.errors.lastName}
                             />
 
-                            <div className="mb-2">Zdjęcie profilowe</div>
-                            <div className="input-group mb-3">
-                                <input type="file" className="form-control" id="inputGroupFile01"/>
+                            <div className="mb-2">
+                                <FontAwesomeIcon icon="user-circle" className="me-1"/>
+                                Zdjęcie profilowe
                             </div>
-
-                            <ButtonWithSpinner
-                                content="Zatwierdź zmiany"
-                                onClick={this.onClickSubmitButton}
-                                disabled={
-                                    this.props.ongoingApiCall ||
-                                    this.state.lastName === '' ||
-                                    this.state.firstName === ''
-                                }
-                                ongoingApiCall={this.props.ongoingApiCall}
-                            />
+                            <div className="custom-file mb-3 mx-auto">
+                                <input type="file" className="custom-file-input col-12 col-sm-10 col-md-9 col-lg-7 col-xl-6 mx-auto"
+                                       id="inputGroupFile02" style={{cursor: "pointer"}}/>
+                            </div>
+                            <div>
+                                <ButtonWithSpinner
+                                    content="Zatwierdź zmiany"
+                                    onClick={this.onClickSubmitButton}
+                                    disabled={
+                                        this.props.ongoingApiCall ||
+                                        this.state.lastName === '' ||
+                                        this.state.firstName === ''
+                                    }
+                                    ongoingApiCall={this.props.ongoingApiCall}
+                                />
+                            </div>
                         </Container>
                     </div>
                 </Collapse>
@@ -93,6 +102,7 @@ class UserProfilePageCollapse extends Component {
         );
     }
 }
+
 UserProfilePageCollapse.defaultProps = {
     errors: []
 }
